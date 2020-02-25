920e1d74ceeb0f2c01c434104c3392191a33a402
Braian <00.braian.dev@gmail.com>
2020-02-25 03:29:58 -0300

    Nodejs study in Freecodecamp

---
+++ Scripting Language Study/Node.js Study/Nodejs and Express/myApp.js
@@ -0,0 +1,90 @@
+
+var express = require('express');
+var app = express();
+
+var bodyParser = require("body-parser");
+
+// --> 7)  Mount the Logger middleware here
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+app.use((req, res, next) => {
+  console.log(req.method+" "+req.path+" - "+req.ip);
+  next();
+});
+
+// --> 11)  Mount the body-parser middleware  here
+/** 11) Get ready for POST Requests - the `body-parser` */
+// place it before all the routes !
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/** 1) Meet the node console. */
+console.log('Hello World'); 
+
+/** 2) A first working Express Server */
+// app.get('/', function(req, res){
+//   res.send('Hello Express');
+// })
+
+/** 3) Serve an HTML file */
+app.get('/', function(req, res){
+    res.sendFile(__dirname+"/views/index.html");
+})
+
+/** 4) Serve static assets  */
+app.use(express.static(__dirname+"/public"));
+
+/** 5) serve JSON on a specific route */
+app.get('/json', (req,res) => {
+  if(process.env.MESSAGE_STYLE=="uppercase"){
+    res.json({
+      message: "HELLO JSON"
+    })
+  }
+  else {
+    res.json({
+      message: "Hello json"
+    })    
+  }
+});
+
+/** 6) Use the .env file to configure the app */
+ 
+
+/** 8) Chaining middleware. A Time server */
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({time: req.time});
+});
+
+/** 9)  Get input from client - Route parameters */
+app.get('/:word/echo', (req,res) => {
+  res.json({echo: req.params.word});
+});
+
+/** 10) Get input from client - Query parameters */
+// /name?first=<firstname>&last=<lastname>
+/** 12) Get data form POST  */
+app.route('/name')
+  .get((req,res)=>{
+  var firstname = req.query.first;
+  var lastname = req.query.last;
+  res.json({ name: `${firstname} ${lastname}`});
+  })
+  .post((req,res)=> {
+  
+});
+
+
+
+
+
+
+// This would be part of the basic setup of an Express app
+// but to allow FCC to run tests, the server is already active
+/** app.listen(process.env.PORT || 3000 ); */
+
+//---------- DO NOT EDIT BELOW THIS LINE --------------------
+
+ module.exports = app;
