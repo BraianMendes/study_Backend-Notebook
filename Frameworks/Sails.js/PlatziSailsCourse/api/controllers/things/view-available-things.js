b17654ef520a157f363aa8c773176afc51548fb2
BraianMendes <00.braian.dev@gmail.com>
2020-04-02 23:50:26 -0300

    Keep studying Sails.js

---
+++ Frameworks/Sails.js/PlatziSailsCourse/api/controllers/things/view-available-things.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View available things',
+
+
+  description: 'Display "Available things" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/things/available-things'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // Respond with view.
+    return {};
+
+  }
+
+
+};
---
+++ Frameworks/Sails.js/PlatziSailsCourse/assets/js/pages/things/available-things.page.js
@@ -0,0 +1,30 @@
+parasails.registerPage('available-things', {
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: {
+    //…
+  },
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function() {
+    // Attach any initial data from the server.
+    _.extend(this, SAILS_LOCALS);
+  },
+  mounted: async function() {
+    //…
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+    
+    clickThing: function(thingId){
+      console.log('clicked thing #'+thingId);
+    }
+
+  }
+});
